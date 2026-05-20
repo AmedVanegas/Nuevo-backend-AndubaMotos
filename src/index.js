74a4027c6c07931cce5fa30e868d8d8c132d7ead
Amed vanegas <amedvanegas368@gmail.com>
2026-05-19 19:59:07 -0500

    endpoint

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ const express = require("express");
 
 const app = express();
 
+app.get("/health", function(req,res){
+
+    res.json({
+        msg:"funciona"
+    })
+})
+
 app.listen(3000, function () {
   console.log("server runng on http://localhost:3000");
 });
