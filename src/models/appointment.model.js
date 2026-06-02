9421bee6c54461c032090a366659dfb34b789c35
Amed vanegas <amedvanegas368@gmail.com>
2026-06-02 18:43:58 -0500

    merge fix 3

--- src/models/appointment.model.js
+++ src/models/appointment.model.js
@@ -8,13 +8,9 @@ const AppointmentSchema = new Schema(
             type: Schema.Types.ObjectId,
             ref: 'users',
         },
-<<<<<<< HEAD
+
 
         schedule: {
-
-=======
-        schedule: {
->>>>>>> refs/remotes/origin/main
             type: Date,
             required: [true, 'La fecha de la cita es obligatoria']
         },
